b1913a27acebde94ab1a9110a1c6527743fd31e4
Constantin Bender <constantinbender51@gmail.com>
2025-12-31 22:05:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -341,5 +341,5 @@ def index():
 threading.Thread(target=run_pipeline, daemon=True).start()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
