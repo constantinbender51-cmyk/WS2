69e4369f1a9f5ce27bd0b504d8b73571ee44cd09
Constantin Bender <constantinbender51@gmail.com>
2026-01-03 17:19:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ DATA_URL = "https://raw.githubusercontent.com/first20hours/google-10000-english/
 NUM_WORDS_TO_LOAD = 100       # Reverted to 100
 AUGMENTATIONS_PER_WORD = 200   
 VALIDATION_SPLIT = 0.2
-BATCH_SIZE = 1024              # Changed to 512
+BATCH_SIZE = 4096              # Changed to 512
 EPOCHS = 10                 # Increased epochs for autoregressive convergence
-LATENT_DIM = 512              # Internal state size
-EMBEDDING_DIM = 256
+LATENT_DIM = 256              # Internal state size
+EMBEDDING_DIM = 128
 RANDOM_SEED = 42
 
 # Regularization Hyperparameters
