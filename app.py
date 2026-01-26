9e8d92da5fad34a631c6679817c32f5f3ba0a123
Constantin Bender <constantinbender51@gmail.com>
2026-01-26 13:31:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def build_models_worker():
             df['section'] = df['close'].apply(lambda x: get_bin(x, min_price, bin_size))
             
             # 2. Split Data 70/30
-            split_idx = int(len(df) * 0.70)
+            split_idx = int(len(df) * 0.950)
             train_df = df.iloc[:split_idx].copy()
             test_df = df.iloc[split_idx:].copy()
 
